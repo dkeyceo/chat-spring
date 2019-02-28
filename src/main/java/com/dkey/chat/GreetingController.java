c2e38fc2865d8a25b36e06203259aada34ffb6b0
dkeyceo <dkeyceo@gmail.com>
2019-02-28 23:21:43 +0200

    with dao layer

--- src/main/java/com/dkey/chat/GreetingController.java
+++ src/main/java/com/dkey/chat/GreetingController.java
@@ -1,14 +1,20 @@
 package com.dkey.chat;
 
+import com.dkey.chat.dao.MessageDao;
+import com.dkey.chat.model.Message;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
 import java.util.Map;
 
 @Controller
 public class GreetingController {
+    @Autowired
+    private MessageDao messageDao;
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Map model) {
         model.put("name", name);
@@ -16,7 +22,27 @@ public class GreetingController {
     }
     @GetMapping
     public String main(Map model) {
-        model.put("name","Hello, fellows");
+        Iterable<Message> messages = messageDao.findAll();
+        model.put("messages",messages);
+        return "main";
+    }
+    @PostMapping
+    public String add(@RequestParam String text, @RequestParam String tag,Map model ){
+        Message message = new Message(text, tag);
+        messageDao.save(message);
+        Iterable<Message> messages = messageDao.findAll();
+        model.put("messages",messages);
+        return "main";
+    }
+    @PostMapping("filter")
+    public String add(@RequestParam String filter,Map model ){
+        Iterable<Message> messages;
+        if(filter!=null && !filter.isEmpty()) {
+            messages = messageDao.findByTag(filter);
+        }else{
+            messages = messageDao.findAll();
+        }
+        model.put("messages",messages);
         return "main";
     }
 
---
+++ src/main/java/com/dkey/chat/dao/MessageDao.java
@@ -0,0 +1,10 @@
+package com.dkey.chat.dao;
+
+import com.dkey.chat.model.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageDao extends CrudRepository<Message, Long> {
+    List<Message> findByTag(String tag);
+}
